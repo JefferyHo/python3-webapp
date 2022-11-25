3fedc384a89e71756294ab85bea94125a5d16a74
Jeff <JefferyHo1993@outlook.com>
2022-11-25 20:00:47 +0800

    day 1 :smile:

---
+++ mysql.py
@@ -0,0 +1,47 @@
+import aiomysql
+
+async def create_pool(loop, **kw):
+  logging.info('create database connection pool...')
+
+  global __pool
+
+  __pool = await aiomysql.create_pool(
+    host = kw.get('host', 'localhost'),
+    port = kw.get('port', 3306),
+    user = kw['user'],
+    password = kw['password'],
+    db = kw['db'],
+    charset = kw.get('charset', 'utf8'),
+    autocommit = kw.get('autocommit', True),
+    maxsize = kw.get('maxsize', 10),
+    minsize = kw.get('minsize', 1),
+    loop = loop
+  )
+
+async def select(sql, args, size = None):
+  log(sql, args)
+  global __pool
+
+  with (await __pool) as conn:
+    cur = await conn.cursor(aiomysql.DictCursor)
+    await cur.execute(sql.replace('?', '%s'), args of ())
+    if size:
+      rs = await cur.fetchmany(size)
+    else:
+      rs = await cur.fetchall()
+    await cur.close()
+    logging.info('row returned: %s' % len(rs))
+    return rs
+
+
+async def execute(sql, args):
+  log(sql)
+  with (await __pool) as conn:
+    try:
+      cur = await conn.cursor()
+      await cur.execute(sql.replace('?'. '%s'), args)
+      affected = cur.rowcount
+      await cur.close()
+    except BaseException as e:
+      raise
+    return affected
